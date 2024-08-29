8621ad374a5964ba7208728bc76d51825a636cc6
Daniel <Morgangiermogrimes@gmail.com>
2024-08-29 09:37:45 +0200

    example code works

--- lib/try/ipad_pick_image.dart
+++ lib/try/ipad_pick_image.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -9,13 +11,33 @@ class IpadPickImage extends StatefulWidget {
 }
 
 class _IpadPickImageState extends State<IpadPickImage> {
-  
+   final ImagePicker _picker = ImagePicker();
+      XFile? _image;
+    
+    Future getImage() async {
+        final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    
+        setState(() {
+          _image = image;
+        });
+      }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Pick image'),),
-      body: Container(
-      ),
-    );
+      body: Center(
+            child: _image == null
+                ? const Text('No image selected.')
+                : Image.file(File(_image!.path)),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: getImage,
+            tooltip: 'Pick Image',
+            child: const Icon(Icons.add_a_photo),
+          ),
+        );
+      
+    
   }
 }
